91028f7b1b7fce41f2c258b31b942098f7d3bd05
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2021-03-25 16:11:06 +0100

    :children_crossing: Add examples

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,22 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
 // Defines and runs the CLI
 func Execute() {
+	examples := []string{"  $ shamir split -n 3 -t 2", "  $ shamir combine -t 2"}
+
 	// Define root command
 	rootCommand := &cobra.Command{
 		Use:     "shamir",
 		Short:   "Split and combine secrets using Shamir's Secret Sharing algorithm.",
 		Long:    "Split and combine secrets using Shamir's Secret Sharing algorithm.",
 		Version: "0.1.0",
+		Example: strings.Join(examples, "\n"),
 	}
 
 	// Define commands
